9734a9eff15660feae7ad54990115c0535cd14d4
k <karthickcam30@gmail.com>
2024-12-16 15:01:55 +0530

    Add files via upload

---
+++ k.js
@@ -0,0 +1,13 @@
+// Add scroll-to-section animation
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
